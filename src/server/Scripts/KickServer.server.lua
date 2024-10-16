42759fb456765533cd82e691f77e816a69b7a167
CoolDude657898 <jdmeacha1@gmail.com>
2024-10-16 00:19:52 -0400

    Documentation Updates

--- src/server/Scripts/KickServer.server.lua
+++ src/server/Scripts/KickServer.server.lua
@@ -6,5 +6,3 @@ local kickModuleServer = require(script.KickModuleServer)
 remotes.Kick.OnServerEvent:Connect(function(player, power, verticalAngle, accuracy, kickSetup)
 	kickModuleServer:Kick(power, verticalAngle, accuracy, kickSetup, game.Workspace.Field.Field.LeftUprights)
 end)
-
-kickModuleServer:InitializeKick(70, "Middle")
